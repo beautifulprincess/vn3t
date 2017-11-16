fc0c1f10d31f86336c34abb0b35ffc61168f3675
juliafodor <juliafodor@yahoo.com>
2017-11-16 22:02:31 +0200

    update pie chart

--- js/piechart.js
+++ js/piechart.js
@@ -37,10 +37,35 @@ Highcharts.chart('pie-container', {
 				events:{
 					click: function() {},
                     mouseOver: function(t) {
+						$(this.dataLabel).stop(!0, !0);
 						this.slice(!0, !0, !0);
+						var tx = 0, ty = 0;
+						if (this.slicedTranslation)
+						{
+							tx = this.slicedTranslation.translateX;
+							ty = this.slicedTranslation.translateY;
+						}
+						if (this.dataLabel.org_translateX == undefined)
+							this.dataLabel.org_translateX = this.dataLabel.translateX;
+						if (this.dataLabel.org_translateY == undefined)
+							this.dataLabel.org_translateY = this.dataLabel.translateY;
+
+						var e = { translateX: tx, translateY: ty };
+						var n = { translateX: this.dataLabel.org_translateX + tx, translateY: this.dataLabel.org_translateY + ty };
+						
+                        this.dataLabel.animate(n);
+						this.connector.animate(e);
                     },
                     mouseOut: function() {
+						$(this.dataLabel).stop(!0, !0);
 						this.slice(!1, !0, !0);
+                        var e = { translateX: this.dataLabel.org_translateX, translateY: this.dataLabel.org_translateY };
+                        var t = {
+                            translateX: 0,
+                            translateY: 0
+                        };
+						this.dataLabel.animate(e);
+						this.connector.animate(t);
 					}
 				}
 			}
