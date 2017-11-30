2f8bef2fa23572738158a8cb8bb78b2285a15372
juliafodor <juliafodor@yahoo.com>
2017-11-30 23:22:02 +0200

    update target

--- index.php
+++ index.php
@@ -340,7 +340,7 @@ Development of Training Materials", 'socials' => array('linkedin' => '#')),
 
 	<div id="pie-container"></div>
 	<div>
-		<p><span class="white">Target:</span> <span class="black">$25 million</span> <span class="white">Supply:</span> <span class="black">4.25  billion tokens</span></p>
+		<p><span class="white">Hard Cap for Crowdsale:</span> <span class="black">55000 Ether</span> <span class="white">Supply:</span> <span class="black">4.25  billion tokens</span></p>
 		<table align=center>
 			<tr><td><span class="ico-percent-color color1"></span></td><td><span class="white">76.8% Community:</span> <span class="black">3 250 000 000</span></td></tr>
 			<tr><td><span class="ico-percent-color color2"></span></td><td><span class="white">5.8 % Affiliate program:</span> <span class="black">250 000 000</span></td></tr>
